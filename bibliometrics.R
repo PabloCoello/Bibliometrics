162823c49db3b4d7c81a59d53e8a80ceaf57daf8
Pablo C <pablo.coello@outlook.es>
2019-10-31 22:44:30 +0100

    Update bibliometrics.R

--- bibliometrics.R
+++ bibliometrics.R
@@ -1,16 +1,33 @@
 library(readxl)
+library(tidyr)
+library(stringr)
 
 setwd("~/GitHub/data")
 Bibliografia <- read_excel("Bibliografia.xlsx")
 
+get_author_list = function(x){
+  authors = strsplit(Bibliografia$Autores, '.,', fixed = TRUE)
+  authors = lapply(authors, str_remove_all, pattern = "[& ]")
+  return(authors)
+}
+
 ref_summary = function(x){
   articles = nrow(x)
   sources = length(unique(x$Revista))
-  period = paste(min(x$Año), '-', max(x$Año))
+  period = paste(min(x$Año, na.rm = TRUE), '-', max(x$Año))
+  citations = mean(x$Citas, na.rm = TRUE)
+  authors = get_author_list(Bibliografia)
+  num_authors = length(unique(unlist(authors)))
+  
   print(paste('Number of articles:', articles))
   print(paste('Number of sources:', sources))
   print(paste('Time period:', period))
-  return(list(articles, sources, period))
+  print(paste('Average citations:', citations))
+  print(paste('Number authors:', num_authors))
+  return(list(articles, sources, period, citations))
 }
 
 sum= ref_summary(Bibliografia)
+authors = get_author_list(Bibliografia)
+
+length(unique(unlist(authors)))
