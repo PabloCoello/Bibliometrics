5c3d1293269ca62a29945cae4dc63db38c7536f6
Pablo Coello <pablo.coello@outlook.es>
2019-11-17 20:56:08 +0100

    update

--- pdf-reference-extract.py
+++ pdf-reference-extract.py
@@ -1,2 +1,12 @@
 import re
+import PyPDF2
 
+with open("paper_example.pdf", mode="rb") as file:
+    pdf_reader = PyPDF2.PdfFileReader(file)
+    text = []
+    for i in range(pdf_reader.numPages):
+        page = pdf_reader.getPage(i)
+        text.append(page.extractText())
+
+
+print(text)
--- scopusapi.py
+++ scopusapi.py
@@ -2,15 +2,49 @@ from elsapy.elsclient import ElsClient
 from elsapy.elsprofile import ElsAuthor, ElsAffil
 from elsapy.elsdoc import FullDoc, AbsDoc
 from elsapy.elssearch import ElsSearch
+import scholarly
 
 client = ElsClient("688f324bf0bbe4273de2fd6ef18593b3")
-print(dir(client))
+keywords = 'SFA + DEA'
 
-doc_srch = ElsSearch("AFFIL(dartmouth) AND AUTHOR-NAME(lewis) AND PUBYEAR > 2011",'scopus')
-doc_srch.execute(client, get_all = True)
-print ("doc_srch has", len(doc_srch.results), "results.")
+def get_search(keywords, client):
+    doc_srch = ElsSearch(keywords,'scopus')
+    doc_srch.execute(client, get_all = True)
+    print ("doc_srch has", len(doc_srch.results), "results.")
+    df = doc_srch.results_df
+    return(df)
+
+df = get_search(keywords, client)
+
+search_query = scholarly.search_keyword('SFA')
+
+def get_citated_by_list(df):
+    pub = []
+    for i in range(len(df)):
+        search_query = scholarly.search_author(df['dc:creator'][i])
+        author = next(search_query).fill()
+        pub.append(author.publications[author.publications == df['dc:title'][i]].fill())
+    return(pub)
+
+citations = get_citated_by_list(df)
 
 
-aff_srch = ElsSearch('affil(ourense)','affiliation')
-aff_srch.execute(client)
-print ("aff_srch has", len(aff_srch.results), "results.")
+search_query = scholarly.search_pubs_query(df['dc:title'][0])
+doc = next(search_query).fill()
+search_query.get_citedby()
+
+
+# Retrieve the author's data, fill-in, and print
+search_query = scholarly.search_author(df['dc:creator'][0])
+author = next(search_query).fill()
+print(author)
+
+# Print the titles of the author's publications
+print([pub.bib['title'] for pub in author.publications])
+
+# Take a closer look at the first publication
+pub = author.publications[author.publications == df['dc:title'][0]].fill()
+print(pub)
+
+# Which papers cited that publication?
+print([citation.bib['title'] for citation in pub.get_citedby()])
