2378ed4cc07a87efb55d9a4ef398b1b693a502c0
PabloC <pablo.coello@outlook.es>
2020-02-24 12:15:53 +0100

    bibliometrics.R updated with code examples, bibliometric_analyser updated with bibliometrix methods

--- bibliometrics.R
+++ bibliometrics.R
@@ -17,8 +17,24 @@ if(!require(rscopus)){install.packages("rscopus");library(rscopus)} # https://gi
 
 setwd("~/GitHub/data/bibliometrics")
 
-bib <- readFiles("EPyRIS.bib")
-bib <- convert2df(bib, dbsource = "wos", format = "bibtex")
+
+bib <- readFiles("scopus.bib")
+bib <- convert2df(bib, dbsource = "scopus", format = "bibtex")
+
+
+results <- biblioAnalysis(bib, sep = ";")
+S<- summary(object=results,k=20,pause=FALSE)
+
+plot(x = results, k = 10, pause = FALSE)
+
+NetMatrix <- biblioNetwork(bib, analysis = "coupling", network = "authors", sep = ";")
+net=networkPlot(NetMatrix, normalize = "salton", weighted=T, n = 20, labelsize=0.5,curved=TRUE,Title = "A Co-citation Network of Authors", type = "kamada", size=TRUE,remove.multiple=TRUE)
+
+CR <- citations(M, field = "article", sep = ";")
+cbind(CR$Cited[1:10])
+
+CR <- citations(M, field = "author", sep = ";")
+cbind(CR$Cited[1:10])
 
 # Con scholar se busca información de los Autores
 
@@ -30,16 +46,20 @@ options("elsevier_api_key" = '688f324bf0bbe4273de2fd6ef18593b3')
 res = author_df(last_name = "Muschelli", first_name = "John", verbose = FALSE, general = FALSE)
 names(res)
 
+df <- scopus_search('multicriteria', api_key = '688f324bf0bbe4273de2fd6ef18593b3', count = 200,
+                    view = c("COMPLETE"), start = 0, verbose = TRUE,
+                    max_count = 200,
+                    http = "https://api.elsevier.com/content/search/scopus",
+                    headers = NULL, wait_time = 0)
 
+ent = gen_entries_to_df(df$entries)
+ent_df <- ent$df
+af_df <- ent$affiliation
+auth_df <- ent$author
+cit_df <- entries_to_citation_df(df$entries)
 
-results <- biblioAnalysis(bib, sep = ";")
-S<- summary(object=results,k=20,pause=FALSE)
-
-plot(x = results, k = 10, pause = FALSE)
-
-NetMatrix <- biblioNetwork(bib, analysis = "coupling", network = "authors", sep = ";")
-net=networkPlot(NetMatrix, normalize = "salton", weighted=T, n = 20, labelsize=0.5,curved=TRUE,Title = "A Co-citation Network of Authors", type = "kamada", size=TRUE,remove.multiple=TRUE)
-
+citations = citation_retrieval(doi = ent_df$`prism:doi`)
+citations = parse_citation_retrieval(citations)
 
 
 # Métodos para plantilla:
@@ -122,3 +142,23 @@ table
 
 cit = get_most_citated_publications(Bibliografia, n=10)
 auth = get_most_citated_authors(Bibliografia, n=10)
+
+
+```{r Scopus query, eval=FALSE, include=FALSE}
+# Doc en https://dev.elsevier.com/documentation/FullTextEntilementAPI.wadl
+# Api limits https://dev.elsevier.com/api_key_settings.html
+df <- generic_elsevier_api(query = NULL, type = c("search", "article",
+                                                  "entitlement", "recommendation", "object", "fragment", "abstract",
+                                                  "affiliation", "embase", "author", "serial", "nonserial", "subject",
+                                                  "holdings", "citation-count", "citations", "metadata", "ev",
+                                                  "ev_records", "analytics"), search_type = c("affiliation", "author",
+                                                                                              "scopus", "scidir", "scidir-object", "sciencedirect", "plumx"),
+                           api_key = NULL, headers = NULL, content_type = c("content",
+                                                                            "feedback"), root_http = "https://api.elsevier.com", http_end = NULL,
+                           verbose = TRUE, api_key_error = TRUE, ...)
+df <- scopus_search(query, api_key = NULL, count = 200,
+                    view = c("STANDARD", "COMPLETE"), start = 0, verbose = TRUE,
+                    max_count = 20000,
+                    http = "https://api.elsevier.com/content/search/scopus",
+                    headers = NULL, wait_time = 0, ...)
+```
